87e89bbb4d64700c4eec5093d164637bec6ce361
Scott Talbot <s@chikachow.org>
2016-03-26 10:48:09 +1100

    hack

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Git2",
     dependencies: [
-      .Package(url: "../CGit2", Version(0, 0, 0)),
+      .Package(url: "https://github.com/cysp/swift-CGit2.git", Version(0, 0, 0)),
     ]
 )
